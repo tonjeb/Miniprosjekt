9d7a68ac5f21f4c20e7818f687528efddbe9423c
olagrasli <ola.grasli@gmail.com>
2013-09-18 16:41:03 +0200

    MenuBar

--- Miiniprosjekt/src/Main.java
+++ Miiniprosjekt/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javax.swing.JFrame;
 
 /**
@@ -9,9 +12,10 @@ import javax.swing.JFrame;
  *
  */
 public class Main extends JFrame {
-
+	static ResourceBundle messages;
 	public Main () {
 		super("Hello World");
+		setJMenuBar (new MenuBar());
 		//add(new Test());
 		pack(); // makes window fit all components
 		setVisible(true);
@@ -21,7 +25,17 @@ public class Main extends JFrame {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale currentLocale;
+		
+		if (args.length==2) {
+			currentLocale = new Locale (args[0], args[1]);	// Språk, land
+		} else if (args.length==1) {
+			currentLocale = new Locale (args[0]);	// språk
+		} else
+			currentLocale = Locale.getDefault();
+			
+		messages = ResourceBundle.getBundle ("I18N", currentLocale);
+		
 		new Main();
 	}
 
