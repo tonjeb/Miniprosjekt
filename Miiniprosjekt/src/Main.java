76d553c05c8e440e20ee6d3c98384a5cfe19663e
olagrasli <ola.grasli@gmail.com>
2013-09-18 16:11:07 +0200

    Main start

--- Miiniprosjekt/src/Main.java
+++ Miiniprosjekt/src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.JFrame;
+
 /**
  * 
  */
@@ -6,14 +8,21 @@
  * @author Ola
  *
  */
-public class Main {
+public class Main extends JFrame {
 
+	public Main () {
+		super("Hello World");
+		//add(new Test());
+		pack(); // makes window fit all components
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Main();
 	}
 
 }
